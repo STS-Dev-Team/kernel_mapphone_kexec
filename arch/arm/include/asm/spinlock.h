3a14332333a541d7ee9d66ff8d82606b3532b18c
Hashcode <hashcode0f@gmail.com>
2012-07-16 23:48:28 -1000

    fixing headers for 3.0.31

--- arch/arm/include/asm/spinlock.h
+++ arch/arm/include/asm/spinlock.h
@@ -86,9 +86,6 @@ static inline void arch_spin_lock(arch_spinlock_t *lock)
 	WFE("ne")
 "	strexeq	%0, %2, [%1]\n"
 "	teqeq	%0, #0\n"
-#if __LINUX_ARM_ARCH__ >= 7
-"	dmb\n"
-#endif
 "	bne	1b"
 	: "=&r" (tmp)
 	: "r" (&lock->lock), "r" (1)
@@ -148,9 +145,6 @@ static inline void arch_write_lock(arch_rwlock_t *rw)
 	WFE("ne")
 "	strexeq	%0, %2, [%1]\n"
 "	teq	%0, #0\n"
-#if __LINUX_ARM_ARCH__ >= 7
-"	dmb\n"
-#endif
 "	bne	1b"
 	: "=&r" (tmp)
 	: "r" (&rw->lock), "r" (0x80000000)
@@ -217,9 +211,6 @@ static inline void arch_read_lock(arch_rwlock_t *rw)
 "	strexpl	%1, %0, [%2]\n"
 	WFE("mi")
 "	rsbpls	%0, %1, #0\n"
-#if __LINUX_ARM_ARCH__ >= 7
-"	dmb\n"
-#endif
 "	bmi	1b"
 	: "=&r" (tmp), "=&r" (tmp2)
 	: "r" (&rw->lock)
