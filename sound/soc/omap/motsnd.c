1728a2aca575bb613ddd9291860a82bb093dcb7c
Jon Pry <jonpry@gmail.com>
2012-07-31 22:55:49 -0400

    Add ABE bypass

--- sound/soc/omap/motsnd.c
+++ sound/soc/omap/motsnd.c
@@ -36,6 +36,7 @@
 #include <plat/clock.h>
 #include "../../../arch/arm/mach-omap2/board-mapphone.h"
 
+#define ABE_BYPASS
 #define MOTSND_CONFIG_ENABLE_ABE
 #define MOTSND_CONFIG_ENABLE_SPDIF
 
@@ -500,7 +501,20 @@ struct snd_soc_dsp_link fe_lp_media = {
 
 /* Digital audio interface glue - connects codec <--> CPU */
 static struct snd_soc_dai_link motsnd_dai[] = {
-#ifdef MOTSND_CONFIG_ENABLE_ABE
+//#ifdef MOTSND_CONFIG_ENABLE_ABE
+#ifdef ABE_BYPASS
+{
+	.name = "Multimedia",
+	.stream_name = "McBSP2-STDac",
+	.cpu_dai_name = "omap-mcbsp-dai.1",
+	.codec_dai_name = "cpcap stdac",
+	.platform_name = "omap-pcm-audio",
+	.codec_name = "cpcap_audio",
+	.init = motsnd_cpcap_init,
+	.ops = &motsnd_ops,
+	.ignore_suspend = 1,
+},
+#endif
 {
 	.name = "Multimedia LP",
 	.stream_name = "Multimedia",
@@ -513,19 +527,9 @@ static struct snd_soc_dai_link motsnd_dai[] = {
 //	.fe_playback_channels = 2,
 	.ignore_suspend = 1,
 },
-#else
-{
-	.name = "Multimedia",
-	.stream_name = "McBSP2-STDac",
-	.cpu_dai_name = "omap-mcbsp-dai.1",
-	.codec_dai_name = "cpcap stdac",
-	.platform_name = "omap-pcm-audio",
-	.codec_name = "cpcap_audio",
-	.init = motsnd_cpcap_init,
-	.ops = &motsnd_ops,
-	.ignore_suspend = 1,
-},
-#endif /*ENABLE_ABE*/
+//#else
+
+//#endif /*ENABLE_ABE*/
 {
 	.name = "Voice",
 	.stream_name = "McBSP3-Codec",
